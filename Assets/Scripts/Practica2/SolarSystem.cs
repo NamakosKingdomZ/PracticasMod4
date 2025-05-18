527571e2b181eda9ba7b38d16c7721e06ce95159
DESKTOP-DVLOBJQ\Carlos <capegiss15@gmail.com>
2025-05-18 20:53:02 +0200

    He hecho toda la practica, he llegado a hacer que se miren y indiquen lineas de distintos colores entre si mientras rotan, no me ha quedado ni fluido ni perfecto, pero creo que para ser una práctica, he entendido el concepto.

--- Assets/Scripts/Practica2/SolarSystem.cs
+++ Assets/Scripts/Practica2/SolarSystem.cs
@@ -9,9 +9,16 @@ public class SolarSystem : MonoBehaviour
     public Transform sunTransform;
     public float speedRotation;
     public float speedRotationSun;
+    public float speedRotationSatelite1;
+    public float speedRotationSatelite2;
+    public Transform satelite1Transform;
+    public Transform satelite2Transform;
+    public Transform cameraTransform;
 
     //Private Attributes
     private float rotationAngle = 30;
+    private float satelite1Angle = 90;
+    private float satelite2Angle = 45;
     
     void Start()
     {
@@ -21,13 +28,21 @@ public class SolarSystem : MonoBehaviour
     void Update()
     {
         //Look At
-        transform.LookAt(centerTransform);
-        
+        sunTransform.LookAt(centerTransform);
+        satelite2Transform.LookAt(satelite1Transform);
+        cameraTransform.LookAt(sunTransform.position);
 
+        //DrawLine
+        Debug.DrawRay(sunTransform.position, centerTransform.position, Color.green);
+        Debug.DrawRay(sunTransform.position, satelite1Transform.position, Color.red);
+        Debug.DrawRay(sunTransform.position, satelite2Transform.position, Color.blue);
+        Debug.DrawRay(satelite1Transform.position, satelite2Transform.position, Color.yellow);
 
 
         //Rotate Around
-        sunTransform.RotateAround(centerTransform.position, Vector3.up, rotationAngle * Time.deltaTime * speedRotationSun);
+        sunTransform.RotateAround(centerTransform.position, Vector3.forward, rotationAngle * Time.deltaTime * speedRotationSun);
+        satelite1Transform.RotateAround(sunTransform.position, Vector3.up, satelite1Angle * Time.deltaTime * speedRotationSatelite1);
+        satelite2Transform.RotateAround(satelite1Transform.position, Vector3.right, satelite2Angle * Time.deltaTime * speedRotationSatelite2);
 
      
         
